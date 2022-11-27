0ac0e8cece23a41dabb172d7807d93713fcaa94d
Tora <109001487+salamovav@users.noreply.github.com>
2022-11-27 19:29:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// let a=2;
+// let b=2;
+// if (a+b>15){
+//     console.log('Да');
+// }else if(a+b<=15){
+//     console.log('Нет');
+// }
+
+let a='3';
+let b=3;
+if(a===b){
+    console.log('true');
+}else {
+    console.log(false);
+}
